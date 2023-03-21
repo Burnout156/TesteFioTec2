c161159b385f3a96dbb2f5337b0d9d8a5b09358f
Bruno Ferreira <brunoferreira378@gmail.com>
2023-03-21 15:38:14 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using TesteFioTecBiblioteca.Services;
+
+string connectionString = "Data Source=myServerAddress;Initial Catalog=myDataBase;User ID=myUsername;Password=myPassword";
+string palavrasFile = "Palavras.txt";
+string emailMessagesTable = "EmailMessages";
+string palavrasChaveTable = "PalavrasChave";
+double minFrequency = 0.05;
+
+// Carrega palavras do dicionário
+HashSet<string> dicionario = new HashSet<string>(File.ReadAllLines(palavrasFile), StringComparer.OrdinalIgnoreCase);
+
+DicionarioServices dicionarioService = new DicionarioServices();
+
+dicionarioService.VerificarPalavra();
+
+
